717465b30726567d6c00d3e3276aff5a5cfc2d8c
Csav_R <116081238+CsavRR@users.noreply.github.com>
2024-09-17 11:16:25 +0200

    KÉSZ

---
+++ oszthato3-mal/oszthato3-mal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oszthato3_mal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 21; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i + ": A szám osztható 3-mal");
+                }
+                else
+                {
+                    Console.WriteLine(i + ": A szám nem osztható 3-mal");
+                }
+            
+            }
+            Console.ReadKey();
+        
+        }
+    }
+}
